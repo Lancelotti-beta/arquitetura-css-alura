3fdbb097af941207970ec8e0fc28247cac9b1824
Lancelotti-beta <brunojosemarques.pe@gmail.com>
2022-06-01 19:10:59 -0300

    Atualizando a nomeação com BEM| Ajuste no js, BEM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-const listaDeLink = document.querySelectorAll('.menu-link');
+const listaDeLink = document.querySelectorAll('.menu__link');
 
 for(let i = 0; i < listaDeLink.length; i++) {
     const link = listaDeLink[i];
     const ativa = link.classList;
 
     link.addEventListener('click', () => {
-        ativa.toggle('ativo');
+        ativa.toggle('menu__link--ativo')
     })
 }
 
